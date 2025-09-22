5a22f9ca0fe85f8c0f65d78fcff41d744677fe95
QoderAI <qoder_ai@qoder.com>
2025-09-22 03:02:40 +0000

    feat: upgrade java version to 21
    Co-authored-by: demo078 <demo078@qoder.com>

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World! Java 21 is working correctly!");
+        System.out.println("Java version: " + System.getProperty("java.version"));
+        System.out.println("Java home: " + System.getProperty("java.home"));
+    }
+}
